2efa253ad43b47ac157d2c0fcedc92fef64063fc
Sanad Kadu <41390064+sanadKadu@users.noreply.github.com>
2021-02-18 20:11:42 +0530

    modified:   contracts/ERC20.sol 	new file:   contracts/ERC20I.sol 	deleted:    contracts/WHIZ.sol

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -1,35 +1,87 @@
-pragma solidity ^0.8.0
+pragma solidity >=0.7.0 <0.9.0;
 
-interface ERC20 {
+import "./ERC20I.sol";
 
-		// Returns total tokens that can exist.
-		funtion totalSupply() external view returns (uint256);\
+contract ERC20 is ERC20I {
+		// Using a hash table to store all balances as
+		// also because using hash table is cheaper interms of gas than expensive array operations.
+		mapping (address => uint256) private _balances;
+		
+		// Mapping the allowances.
+		mapping (address => mapping (address => uint256)) private _allowances;
 
-		// Returns amount of Tokens owned by some wallet/account.
-		function balanceOf(address account) external view returns (uint256);
+		string private _name;
+		string private _symbol;
+		uint256 private totalSupply;
 
-		// Transfers "amount" of tokens from callers account to the
-		// supplied recipients account and returns boolean for status
-		// of the transfer.
-		function transfer(address recipient, uint256 amount) external returns (bool);
+		// As contructor is called only once (sabse pehli bar jab EVM pe execute hota hai tab)
+		// So  we'll use it to set Name and Symbol.
+		constructor (string memory name, string memory symbol){
+				_name = name;
+				_symbol = symbol;
+		}
 
-		// Returns the allowed amount of the tokens that the spender can spend
-		// on-behalf of the owner. As per standard it is 0  by default and
-		// can be changed when {approve} or {transferFrom} functions are called.
-		function allowance(address owner, address spender) external view returns (uint256);
+		// Returns name of the token.
+		function getName() public view returns (string memory){
+				return _name;
+		}
 
-		// This function sets the allowance of the spender over the callers
-		// tokens and returns a boolean indicating the status of operation.
-		function approve(address spender, uint256 amount) external returns (bool);
+		// Returns Symbol of the token.
+		function getSymbol() public view returns (string memory){
+				return _symbol;
+		}
 
-		// Transfers "amount" of tokens from sender to recipient as per the above
-		// allowance mechanism. Also "amount" is deducted from the callers allowance.
-		function transferFrom(address sender, address recipient, uint25 amount) external returns (bool);
+		fuction getTotalSupply() public view override returns (uint256){
+				return totalSupply;
+		}
 
-		// Emmited when "value" tokens are moved from address "from" toaddress "to".
-		event Transfer(address indexed from, address indexed to, uint256 value);
+		// Now we implement the ERC20 Standard mandatory functions.
+		function allowance(address owner, address spender) public view override returns (uint256) {
+				return _allowances[owner][spender];
+		}
 
-		// Emitted when allowance of "spender" for an "owner" is set by a call
-		// to {approve} where "value" is the new allowance.
-		event Approval (address indexed owner, address indexed spender, uint256 value);
+		function transfer(address recipient, uint256 amount) public returns (bool) {
+				_transfer(msg.sender, recipient, amount);
+				return true;
+		}
+
+		function approve(address sender, uint256 amount) public override returns (bool) {
+				_approve(msg.sender, spender, amount);
+		}
+
+		function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+				_transfer(sender, reicipient, amount);
+				uint256 currentAllowance = _allowances[sender][msg.sender];
+				require(currentAllowance >= amount, "Transfer exceeds allowance");
+				_approve(sender, msg.sender, currentAllowance - amount);
+				return true;
+		}
+
+		function _transfer(address sender, address reciever, uint256 amount) internal {
+				require(sender != address(0), "Can't transfer from 0 address");
+				require(reciever != address(0), "Can't tansfer to 0 address");
+				uint256 senderBalance = _balances[sender];
+				require(senderBalance >= amount, "Not enough balance!");
+				_balances[sender] = senderBalance - amount;
+				_balances[reciever] += amount;
+			 
+				emit Transfer(sender, reciever, amount);
+		}
+
+		// Creates "amount" of tokens and adds them to "account" thus increasing
+		// the total supply.
+		function _mint(address account, uint256 amount) internal virtual {
+				require(account != address(0), "Can't mint to 0 address");
+				totalSupply += amount;
+				_balances[account] += amount;
+				emit Transfer(address(0), account, amount);
+		}
+
+		function _approve(address owner, address spender, uint256 amount) internal virtual {
+				require(owner != address(0), "Can't approve from 0 address");
+				require(spender != address(0), "Can't approve to 0 address");
+				_allowances[owner][spender] = amount;
+				emit Approval(owner, spender, amount);
+		}
+		
 }
---
+++ contracts/ERC20I.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.8.0
+
+interface ERC20I {
+
+		// Returns total tokens that can exist.
+		funtion totalSupply() external view returns (uint256);\
+
+		// Returns amount of Tokens owned by some wallet/account.
+		function balanceOf(address account) external view returns (uint256);
+
+		// Transfers "amount" of tokens from callers account to the
+		// supplied recipients account and returns boolean for status
+		// of the transfer.
+		function transfer(address recipient, uint256 amount) external returns (bool);
+
+		// Returns the allowed amount of the tokens that the spender can spend
+		// on-behalf of the owner. As per standard it is 0  by default and
+		// can be changed when {approve} or {transferFrom} functions are called.
+		function allowance(address owner, address spender) external view returns (uint256);
+
+		// This function sets the allowance of the spender over the callers
+		// tokens and returns a boolean indicating the status of operation.
+		function approve(address spender, uint256 amount) external returns (bool);
+
+		// Transfers "amount" of tokens from sender to recipient as per the above
+		// allowance mechanism. Also "amount" is deducted from the callers allowance.
+		function transferFrom(address sender, address recipient, uint25 amount) external returns (bool);
+
+		// Emmited when "value" tokens are moved from address "from" toaddress "to".
+		event Transfer(address indexed from, address indexed to, uint256 value);
+
+		// Emitted when allowance of "spender" for an "owner" is set by a call
+		// to {approve} where "value" is the new allowance.
+		event Approval (address indexed owner, address indexed spender, uint256 value);
+}
